f4f15ab4ee122b048c0ed98409da2f80182e4336
Simon Knott <skn0tt@Simons-MacBook-Pro.local>
2017-09-08 17:33:31 +0200

    Hanoi Works

--- Rekursion/src/Fibonacci.java
+++ Rekursion/src/Fibonacci.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -6,31 +7,51 @@ import java.util.Scanner;
  * Created by simon.knott on 08.09.2017.
  */
 public class Fibonacci {
-    static List<Integer> cache = new ArrayList<>();
-    public static void main(String[] args){
-        // cache Setup
-        cache.add(0);
-        cache.add(1);
+    private static String GREETING =
+            "Fibonacci Calculator by Skn0tt.\n" +
+            "Do you want to use the cached variant? [true/false]";
+    private static String INPUT =
+            "n = ";
+    private static String OUTPUT =
+            "fib: ";
+    private static String TIME =
+            "Laufzeit: ";
 
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        //int n = s.nextInt();
+
+        System.out.println(GREETING);
+        boolean cached = s.nextBoolean();
+        System.out.print(INPUT);
+        int n = s.nextInt();
+        if (n < 0) throw new IllegalArgumentException("Illegal Argument N!");
+        System.out.print(OUTPUT);
 
         long start = System.currentTimeMillis();
-
-        for (int i = 0; i < 50; i++) {
-            System.out.println(fibRecCache(i));
-        }
-
+        System.out.println(cached ? fibRecCache(n) : fibRec(n));
         long stop = System.currentTimeMillis();
+
+        System.out.print(TIME);
         System.out.println(stop - start);
     }
 
+    /**
+     * Recursive Fibonacci Implementation
+     * @param n Index in Fibonacci row
+     * @return fib(n)
+     */
     static int fibRec(int n) {
         return n <= 2 ? 1 : fibRec(n - 1) + fibRec(n - 2);
     }
 
+    /**
+     * Recursive Fibonacci Implementation with Cache
+     * @param n Index in Fibonacci row
+     * @return fib(n)
+     */
     static int fibRecCache(int n){
         if (cache.size() <= n) cache.add(fibRecCache(n - 1) + fibRecCache(n - 2));
         return cache.get(n);
     }
+    static ArrayList<Integer> cache = new ArrayList<>(Arrays.asList(0, 1));
 }
--- Rekursion/src/Hanoi.java
+++ Rekursion/src/Hanoi.java
@@ -4,16 +4,35 @@ import java.util.Scanner;
  * Created by simon.knott on 08.09.2017.
  */
 public class Hanoi {
+    private static String GREETING =
+            "Hanoi Calculator by Skn0tt.";
+    private static String INPUT =
+            "n = ";
+    private static String OUTPUT =
+            "Züge: ";
+    private static String TIME =
+            "Laufzeit: ";
+
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
 
+        System.out.println(GREETING);
+        System.out.print(INPUT);
         int n = s.nextInt();
+        if (n < 0) throw new IllegalArgumentException("Illegal Argument N!");
+        System.out.print(OUTPUT);
 
+        long start = System.currentTimeMillis();
         System.out.println(hanoiRec(n));
+        long stop = System.currentTimeMillis();
+
+        System.out.print(TIME);
+        System.out.println(stop - start);
     }
 
     /**
      * Recursive Hanoi Implementation
+     * Idea: Z(n) = 2 * Z(n-1) + 1
      * @param n number of disks
      * @return number of turns needed
      */
@@ -23,6 +42,7 @@ public class Hanoi {
 
     /**
      * Explicit Hanoi Implementation
+     * Idea: Z(n) = 2^n - 1
      * @param n number of disks
      * @return number of turns needed
      */
@@ -32,18 +52,14 @@ public class Hanoi {
 
     /**
      * Iterative Hanoi Implementation
+     * Idea: Z(n) = 2 * Z(n-1) + 1
      * @param n number of disks
      * @return number of turns needed
      */
-    /*
     static int hanoiIt(int n) {
-        int vorletzter;
-        int letzter;
-        for (int i = 0; i < n; i++) {
+        int last = 0;
+        for (int i = 0; i < n - 1; i++) last = 2 * last + 1;
 
-        }
-
-        return count + 1;
+        return 2 * last + 1;
     }
-    */
 }
